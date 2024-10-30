f6540b105a88311ad42efc4caf074f58c3c26dba
Wallram123 <nilsson.walle@gmail.com>
2024-10-30 10:24:42 +0200

    fsfsf

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Install packages
-python3 -m pip install -r requirements.txt || exit 1
+python3 -m pip install -r requirements.txt
 
-# Apply database migrations
-python3 manage.py makemigrations --noinput || exit 1
-python3 manage.py migrate --noinput || exit 1
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
-# Collect static files
-python3 manage.py collectstatic --noinput || exit 1
+python3 manage.py collectstatic --noinput
