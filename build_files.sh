72dab048fb3f13a2d9b271f7dc91c5bea57681c2
Wallram123 <nilsson.walle@gmail.com>
2024-10-30 09:30:50 +0200

    asd

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
